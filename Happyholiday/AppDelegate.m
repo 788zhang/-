c9a91cacfcb025fad4dcb1fd673f5396ee6c1928
张鹏飞-芒果iOS <850944623@qq.com>
2016-01-04 15:05:10 +0800

    张鹏飞

--- Happyholiday/AppDelegate.m
+++ Happyholiday/AppDelegate.m
@@ -18,6 +18,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    
+    UITabBarController *tabbar=[[UITabBarController alloc]init];
+    
+    
+    
+    
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
